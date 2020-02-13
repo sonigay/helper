e4fe5861560a3bb375c239a8633c86e498c1f080
sonigay <43840048+sonigay@users.noreply.github.com>
2020-02-13 12:11:14 +0900

    Update test.py

--- test.py
+++ test.py
@@ -82,11 +82,17 @@ async def on_message(message):
         wks = gc.open('오전재고').worksheet('전월실적출력')
         wks.update_acell('A1', SearchID)
         result = wks.acell('B1').value
+        result2 = wks.acell('C1').value
         
         embed = discord.Embed(
             title = ' 📈 ' + SearchID + ' 전월실적! ',
-            description= '**```css\n' + SearchID + '2ND/중고/선불개통제외 전월마감실적 입니다.\n중도 취소발생시 실적에서 차이가 생길수 있습니다.\n'+ result + '한달동안 고생 많으셨습니다. ```**',
+            description= '**```css\n' + SearchID + '2ND/중고/선불개통제외 전월마감실적 입니다.\n중도 취소발생시 실적에서 차이가 생길수 있습니다.\n'+ result + ' ```**',
             color=0x50508C
+            )
+        embed.add_field(
+            name="📈 모델통계 ",
+            value= '**```css\n' + result2 + ' 입니다. 한달동안 고생 많으셨습니다. ```**',
+            inline = False
             )
         await client.send_message(message.channel, embed=embed)
         
@@ -98,12 +104,18 @@ async def on_message(message):
         wks.update_acell('A1', SearchID)
         result2 = wkstime.acell('C1').value        
         result = wks.acell('B1').value
+        result3 = wks.acell('C1').value
 
         
         embed = discord.Embed(
             title = ' 📈 ' + SearchID + ' 당월실적! ',
-            description= '**```css\n' + SearchID + '2ND/중고/선불개통제외 당월실적 입니다.\n마지막 데이터 업로드시간은\n'+ result2 + ' 입니다.' + result + '실시간조회가 아니라서 다소 차이가 있습니다.\n이번달도 끝가지 화이팅입니다!! ```**',
+            description= '**```css\n' + SearchID + '2ND/중고/선불개통제외 당월실적 입니다.\n마지막 데이터 업로드시간은\n'+ result2 + ' 입니다.' + result + ' ```**',
             color=0x50508C
+            )
+        embed.add_field(
+            name="📈 모델통계 ",
+            value= '**```css\n' + result2 + ' 입니다. 실시간조회가 아니라서 다소 차이가 있습니다.\n이번달도 끝가지 화이팅입니다!! ```**',
+            inline = False
             )
         await client.send_message(message.channel, embed=embed)        
         
