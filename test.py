585c597e0e59cd1dc7478af0781024084817f8b5
sonigay <43840048+sonigay@users.noreply.github.com>
2020-02-13 23:18:45 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1032,26 +1032,6 @@ async def on_message(message):
         await client.send_message(message.channel, embed=discord.Embed(description=emoji[randomNum])) # 랜덤 이모티콘을 메시지로 출력합니다.
 
 
-    if message.content.startswith('!타이머'):
-
-        Text = ""
-        learn = message.content.split(" ")
-        vrsize = len(learn)  # 배열크기
-        vrsize = int(vrsize)
-        for i in range(1, vrsize):  # 띄어쓰기 한 텍스트들 인식함
-            Text = Text + " " + learn[i]
-
-        secint = int(Text)
-        sec = secint
-
-        for i in range(sec, 0, -1):
-            print(i)
-            await client.send_message(message.channel, embed=discord.Embed(description='타이머 작동중 : '+str(i)+'초'))
-            time.sleep(1)
-
-        else:
-            print("땡")
-            await client.send_message(message.channel, embed=discord.Embed(description='타이머 종료'))
 
     if message.content.startswith('!제비뽑기'):
         channel = message.channel
